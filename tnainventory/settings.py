a0936e74df05bf5b02fbeebb88885134a96ff419
unknown <dave@tac-llc.com>
2020-03-03 15:24:45 -0600

    launched to heroku, users added, BETA test

--- tnainventory/settings.py
+++ tnainventory/settings.py
@@ -13,7 +13,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')gl^isdg-(jv&n%(!!#8pl@_ucp7oie&g+mc@_l0%px5mb8n2v'
+SECRET_KEY = os.env['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
