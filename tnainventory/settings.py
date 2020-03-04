80813b54a424fab2efd3e5b2ade80214c97be128
unknown <dave@tac-llc.com>
2020-03-04 10:01:04 -0600

    fixed staff_list layout

--- tnainventory/settings.py
+++ tnainventory/settings.py
@@ -3,12 +3,7 @@ import os
 import django_heroku
 import environ
 
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-DEBUG = env('DEBUG')
+DEBUG = True 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates') 
@@ -19,7 +14,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = ')gl^isdg-(jv&n%(!!#8pl@_ucp7oie&g+mc@_l0%px5mb8n2v'
 
 ''' django-environ / .env file for secret keys'''
 
