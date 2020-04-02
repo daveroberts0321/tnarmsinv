e90bed6979d95edfd9e40295a9819385076a0017
unknown <dave@tac-llc.com>
2020-04-02 14:57:06 -0500

    static files

--- tnainventory/settings.py
+++ tnainventory/settings.py
@@ -137,7 +137,9 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
  
 #media
 MEDIA_ROOT = MEDIA_DIR
