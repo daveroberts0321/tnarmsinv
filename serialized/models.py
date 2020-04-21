5cacca17a4b2502fcf21e713c5d91c15663ec1f7
unknown <dave@tac-llc.com>
2020-04-21 10:53:07 -0500

    templates 20200421

--- serialized/models.py
+++ serialized/models.py
@@ -6,27 +6,12 @@ from qr_code.qrcode.utils import ContactDetail
 
 
 ''' .CSV files will be uploaded using https://medium.com/@simathapa111/how-to-upload-a-csv-file-in-django-3a0d6295f624'''
-# Create your models here.
-TYPE = (
-    ('TNARMS15', "TNARMS15"),
-    ('AR308', 'AR308'),
-    ('TAC9', 'TAC9')
-    )
 
-COLOR = (
-    ('BLK', "BLK"),
-    ('FDE', 'FDE'),
-    ('STEALTH', 'STEALTH'),
-    ('OD', 'OD'),
-    ('RIFLE', 'RIFLE'),
-    ('CLEAR', 'CLEAR'),
-    ('PINK', 'PINK')
-)
 
 class Serialized(models.Model):
     """fields for serialized equipment to be used with QR codes. using qr_code: https://django-qr-code.readthedocs.io/en/latest/pages/README.html#usage """
     serialnumber = models.CharField(max_length=20,null=True, default = 'none')
-    gunmasterid = models.CharField(max_length=50,unique = True, null=True, blank = True)
+    gunmasterid = models.CharField(max_length=50, null=True, blank = True)
     modeltype = models.CharField(max_length=50,null=True, default = 'AR15')
     color = models.CharField(max_length=50,null=True, default = 'BLK')
     dateaquired = models.CharField(max_length=15,null=True, default='01-01-2020')
@@ -35,7 +20,8 @@ class Serialized(models.Model):
     ordernumber = models.CharField(max_length=50,null=True, default = '0000', blank = True)
     notes = models.TextField(blank = True, null=True,)
     
-
+    class Meta:
+        ordering = ['modeltype', 'color']
     
     def __str__(self):
         """Unicode representation of Serialized."""
